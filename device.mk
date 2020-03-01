dcde09e0d0154225ef3826fbca40bb91a14cdebf
Ethan Chen <intervigil@gmail.com>
2020-03-01 00:34:32 -0800

    X01AD: Remove unnecessary netutils packages
    * These are already packaged by AOSP.
    
    Change-Id: I77f91fbbb33a6d891d14fe852b094ab914ca3111

--- device.mk
+++ device.mk
@@ -241,12 +241,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
-# Netutils
-PRODUCT_PACKAGES += \
-    netutils-wrapper-1.0 \
-    android.system.net.netd@1.0 \
-    libandroid_net
-
 # Offline charger
 PRODUCT_PACKAGES += \
     charger_res_images \
