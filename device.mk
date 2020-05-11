59f4d61a1aadfafd4c25ce0ae0e31061bb84bcc1
LuK1337 <priv.luk@gmail.com>
2020-05-11 15:40:59 +0200

    X01AD: Build missing audio extn libs
    Change-Id: I61fbed6efaa895cf7458fe9284e2fea912ac48c8
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- device.mk
+++ device.mk
@@ -92,6 +92,13 @@ PRODUCT_PACKAGES += \
     libqcompostprocbundle
 
 # Audio configuration
+PRODUCT_PACKAGES += \
+    libhdmiedid \
+    libhfp \
+    libsndmonitor \
+    libspkrprot \
+    libssrec
+
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
 	$(LOCAL_PATH)/audio/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
