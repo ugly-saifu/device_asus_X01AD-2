9e62309f5f33961c6e12d0fd05b6ff55af9b7c80
nguyenhung9x2020 <nguyenhung9x2018@gmail.com>
2021-09-08 09:23:09 +0700

    X01AD: Update blob from OEM

--- device.mk
+++ device.mk
@@ -122,8 +122,11 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.common@1.0 \
+    android.hardware.camera.device@3.4  \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
+    vendor.qti.hardware.camera.device@1.0 \
     libgui_vendor \
     libstdc++.vendor \
     Snap
