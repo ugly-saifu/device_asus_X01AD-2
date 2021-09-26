22055da45e60aa55d0da059b9416f57fc2afbbbf
nguyenhung9x2020 <nguyenhung9x2018@gmail.com>
2021-09-26 18:15:31 +0700

    X01AD: Update prop camera from stock Q

--- device.mk
+++ device.mk
@@ -124,6 +124,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0 \
     libgui_vendor \
+    libdng_sdk.vendor \
     libstdc++.vendor \
     Snap
 
