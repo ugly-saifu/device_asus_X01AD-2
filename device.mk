3467ebc2d9b4cbd18510b758cc82978a052d4a45
nguyenhung9x2020 <nguyenhung9x2018@gmail.com>
2021-09-27 08:43:59 +0700

    X01AD: fix missing separator.

--- device.mk
+++ device.mk
@@ -21,9 +21,8 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += 
-    $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig \
-    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
