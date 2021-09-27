d0471ce7c675a13a353070f4915f814889e551b7
nguyenhung9x2020 <nguyenhung9x2018@gmail.com>
2021-09-27 08:16:40 +0700

    X01AD: overlay: Exclude Snap overlays from RRO

--- device.mk
+++ device.mk
@@ -21,7 +21,9 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += 
+    $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig \
+    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
